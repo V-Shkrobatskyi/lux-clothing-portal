a5f2d856e23bed08a17232fca7e276e1c188b899
Vitalii <vetali5700gmail.com>
2024-11-30 18:07:14 +0200

    add models to admin panel

--- lux_clothing/admin.py
+++ lux_clothing/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Address,
+    Profile,
+    Order,
+    OrderItem,
+    Product,
+    ProductVariant,
+    Category,
+    Brand,
+    Color,
+    Size,
+    ProductPhoto,
+)
+
+admin.site.register(Address)
+admin.site.register(Profile)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Product)
+admin.site.register(ProductVariant)
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(ProductPhoto)
