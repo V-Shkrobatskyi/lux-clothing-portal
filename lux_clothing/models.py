8f0cfdbd21d93c6fed5aca1f716cc9f2cfd13372
Vitalii <vetali5700gmail.com>
2024-11-30 17:56:02 +0200

    add models: Category, Brand, Color, Size, ProductPhoto, ProductVariant, Product, OrderItem

--- lux_clothing/models.py
+++ lux_clothing/models.py
@@ -1,5 +1,17 @@
+import os
+import uuid
+
+from django.utils.text import slugify
+from colorfield.fields import ColorField
 from django.conf import settings
 from django.db import models
+
+
+def image_file_path(instance, filename):
+    extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.user)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join(f"uploads/{instance.__class__.__name__.lower()}/", filename)
 
 
 class Address(models.Model):
@@ -31,7 +43,68 @@ class Profile(models.Model):
         return str(self.user.full_name)
 
     def __str__(self):
-        return str(self.user.full_name)
+        return self.full_name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    color_hex = ColorField(default="#FF0000")
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+
+class ProductPhoto(models.Model):
+    image = models.ImageField(null=True, upload_to=image_file_path, blank=True)
+
+
+class ProductVariant(models.Model):
+    color = models.ForeignKey(
+        Color, on_delete=models.CASCADE, related_name="product_variant"
+    )
+    size = models.ForeignKey(
+        Size, on_delete=models.CASCADE, related_name="product_variant"
+    )
+    photos = models.ManyToManyField(
+        ProductPhoto, blank=True, related_name="product_variant"
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True
+    )
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=63, unique=True)
+    description = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="product"
+    )
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="product")
+    details = models.TextField()
+    variant = models.ForeignKey(
+        ProductVariant, on_delete=models.CASCADE, related_name="product"
+    )
+
+
+class OrderItem(models.Model):
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="order_item"
+    )
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="order_item"
+    )
+    quantity = models.IntegerField(max_length=12)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 class Order(models.Model):
@@ -49,7 +122,9 @@ class Order(models.Model):
     order_address = models.ForeignKey(
         Address, on_delete=models.CASCADE, related_name="profile"
     )
-    # order_items =
+    order_items = models.ForeignKey(
+        OrderItem, on_delete=models.CASCADE, related_name="order"
+    )
     order_phone_number = models.IntegerField(max_length=12, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=15, choices=StatusChoices.choices)
