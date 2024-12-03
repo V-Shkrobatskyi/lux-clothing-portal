e3ca193392d3f28080d4730bd6640975683909dc
Vitalii <vetali5700gmail.com>
2024-12-03 22:46:01 +0200

    add informative __str__ methods to models

--- lux_clothing/models.py
+++ lux_clothing/models.py
@@ -45,9 +45,16 @@ class Profile(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=63, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Brand(models.Model):
     name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 class ProductHead(models.Model):
     title = models.CharField(max_length=63, unique=True)
@@ -59,6 +66,9 @@ class ProductHead(models.Model):
         Brand, on_delete=models.CASCADE, related_name="product_head"
     )
     details = models.TextField()
+
+    def __str__(self):
+        return f"{self.category}, {self.brand}, {self.title}"
 
 
 class ProductPhoto(models.Model):
@@ -72,10 +82,15 @@ class Color(models.Model):
         ProductPhoto, related_name="color", symmetrical=False
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Size(models.Model):
     name = models.CharField(max_length=63, unique=True)
 
+    def __str__(self):
+        return self.name
 
 class ProductPhoto(models.Model):
     image = models.ImageField(null=True, upload_to=image_file_path, blank=True)
@@ -92,6 +107,8 @@ class Product(models.Model):
     )
     inventory = models.PositiveIntegerField(default=0)
 
+    def __str__(self):
+        return f"{self.product_head}, {self.color}, {self.size}, {self.price}, {self.discount}"
 
 class Product(models.Model):
     title = models.CharField(max_length=63, unique=True)
@@ -115,6 +132,9 @@ class OrderItem(models.Model):
     )
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"product: '{self.product}', quantity: {self.quantity}, total price: {self.quantity * self.product.price}"
 
 
 class Order(models.Model):
@@ -145,4 +165,4 @@ class Order(models.Model):
         ordering = ["-created"]
 
     def __str__(self):
-        return str(self.created)
+        return f"{self.created.__format__('%Y-%m-%d %H:%M:%S')}, {self.profile}, {self.status}, {self.price}"
