d3a1d7211f07d3e4c849080f782b0225613c2c12
Vitalii <vetali5700gmail.com>
2024-12-07 21:12:58 +0200

    add main urls.py in lux_clothing_service with path to urls in lux_clothing and user apps

--- lux_clothing_service/urls.py
+++ lux_clothing_service/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/lux_clothing/", include("lux_clothing.urls", namespace="lux_clothing")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
