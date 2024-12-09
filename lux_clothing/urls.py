bbe5436cc23faec78fa250b6daa7bb7fe023d735
Vitalii <vetali5700gmail.com>
2024-12-09 21:37:29 +0200

    add addresses path

--- lux_clothing/urls.py
+++ lux_clothing/urls.py
@@ -2,11 +2,13 @@ from rest_framework import routers
 
 from lux_clothing.views import (
     ProfileViewSet,
+    AddressViewSet,
 )
 
 app_name = "lux_clothing"
 
 router = routers.DefaultRouter()
 router.register("profiles", ProfileViewSet)
+router.register("addresses", AddressViewSet)
 
 urlpatterns = router.urls
