51a606792f7cedb330420dba08b42f485e3100ba
Vitalii <vetali5700gmail.com>
2024-12-09 22:49:03 +0200

    simplify IsOwnerOrIsAdmin and add IsAdminALLOrReadOnly permission

--- lux_clothing/permissions.py
+++ lux_clothing/permissions.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsOwnerOrIsAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool(
-            (request.user and request.user.is_staff and request.user.is_authenticated)
-            or (obj.user == request.user)
-        )
+        return bool(request.user.is_staff or obj.user == request.user)
+
+
+class IsAdminALLOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.is_staff or request.method in SAFE_METHODS)
