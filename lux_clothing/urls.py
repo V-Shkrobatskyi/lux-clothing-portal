62397e8c6a9ded4538473a714a2f144676a73dc3
Vitalii <vetali5700gmail.com>
2024-12-09 22:49:45 +0200

    add categories, brands and sizes path

--- lux_clothing/urls.py
+++ lux_clothing/urls.py
@@ -3,6 +3,9 @@ from rest_framework import routers
 from lux_clothing.views import (
     ProfileViewSet,
     AddressViewSet,
+    CategoryViewSet,
+    BrandViewSet,
+    SizeViewSet,
 )
 
 app_name = "lux_clothing"
@@ -10,5 +13,8 @@ app_name = "lux_clothing"
 router = routers.DefaultRouter()
 router.register("profiles", ProfileViewSet)
 router.register("addresses", AddressViewSet)
+router.register("categories", CategoryViewSet)
+router.register("brands", BrandViewSet)
+router.register("sizes", SizeViewSet)
 
 urlpatterns = router.urls
