949d33152877fa9fca0a30fad9ac68cb99f71c68
Vitalii <vetali5700gmail.com>
2025-01-11 22:02:33 +0200

    back postgres support and update test_project_data.json with testing data

--- lux_clothing_service/settings.py
+++ lux_clothing_service/settings.py
@@ -87,14 +87,21 @@ WSGI_APPLICATION = "lux_clothing_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": os.environ["POSTGRES_PORT"],
-        "NAME": os.environ["POSTGRES_NAME"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "HOST": os.environ["POSTGRES_HOST"],
+#         "PORT": os.environ["POSTGRES_PORT"],
+#         "NAME": os.environ["POSTGRES_NAME"],
+#         "USER": os.environ["POSTGRES_USER"],
+#         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+#     }
+# }
 
 
 # Password validation
