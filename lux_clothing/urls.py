02ef185bf9536c267072e4fe6011670fffb3552e
Vitalii <vetali5700gmail.com>
2025-01-15 22:09:31 +0200

    add migrations, add path's for ForWhomViewSet and StyleViewSet to project urls

--- lux_clothing/urls.py
+++ lux_clothing/urls.py
@@ -12,6 +12,8 @@ from lux_clothing.views import (
     ProductViewSet,
     OrderItemViewSet,
     OrderViewSet,
+    ForWhomViewSet,
+    StyleViewSet,
 )
 
 app_name = "lux_clothing"
@@ -21,6 +23,8 @@ router.register("profiles", ProfileViewSet)
 router.register("addresses", AddressViewSet)
 router.register("categories", CategoryViewSet)
 router.register("brands", BrandViewSet)
+router.register("for_whom", ForWhomViewSet)
+router.register("styles", StyleViewSet)
 router.register("sizes", SizeViewSet)
 router.register("products_head", ProductHeadViewSet)
 router.register("products_photo", ProductPhotoViewSet)
