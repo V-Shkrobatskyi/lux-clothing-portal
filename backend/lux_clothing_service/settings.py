725b4eee7a2eaec38bc595de0c408f76ad0f4286
Vitalii <vetali5700gmail.com>
2025-02-17 23:14:01 +0200

    fix path for test database db.sqlite3

--- backend/lux_clothing_service/settings.py
+++ backend/lux_clothing_service/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = "lux_clothing_service.wsgi.application"
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#         "NAME": BASE_DIR.parent / "db.sqlite3",
 #     }
 # }
 
