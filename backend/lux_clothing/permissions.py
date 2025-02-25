1d7a262ed307fd0dbb9da4e6fac889e413281fcf
Vitalii <vetali5700gmail.com>
2025-02-25 22:51:31 +0200

    modify classes checks in permissions.py for reduce SQL requests

--- backend/lux_clothing/permissions.py
+++ backend/lux_clothing/permissions.py
@@ -1,45 +1,41 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
-from lux_clothing.models import Profile
-
 
 class IsOwnerOrIsAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool(request.user.is_staff or obj.user == request.user)
+        return request.user.is_staff or obj.user == request.user
 
 
 class IsAddressOwnerOrIsAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool(
-            request.user.is_staff or obj in request.user.profile.addresses.all()
+        return (
+            request.user.is_staff
+            or obj.profiles.filter(id=request.user.profile.id).exists()
         )
 
 
 class IsAdminALLOrOwnerCanPostAndGet(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool(
-            request.user.is_staff
-            or (obj.user == request.user and request.method in ["POST", "GET"])
+        return request.user.is_staff or (
+            obj.user == request.user and request.method in ["POST", "GET"]
         )
 
 
 class IsAdminALLOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user.is_staff or request.method in SAFE_METHODS)
+        return request.user.is_staff or request.method in SAFE_METHODS
 
 
 class IsAdminALLOrHasProfile(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user.is_staff or Profile.objects.filter(user=request.user))
+        return request.user.is_staff or hasattr(request.user, "profile")
 
 
 class HasProfile(BasePermission):
     def has_permission(self, request, view):
-        return bool(Profile.objects.filter(user=request.user))
+        return hasattr(request.user, "profile")
 
 
 class IsAuthenticatedAndHasProfile(BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.user.is_authenticated and Profile.objects.filter(user=request.user)
-        )
+        return request.user.is_authenticated and hasattr(request.user, "profile")
