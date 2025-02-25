3a8dab3ee5ee31714b23eea7aa286979f90b2048
Vitalii <vetali5700gmail.com>
2025-02-25 22:52:50 +0200

    add path for django-debug-toolbar

--- backend/lux_clothing_service/urls.py
+++ backend/lux_clothing_service/urls.py
@@ -24,19 +24,28 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
+from debug_toolbar.toolbar import debug_toolbar_urls
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/user/", include("user.urls", namespace="user")),
-    path("api/lux_clothing/", include("lux_clothing.urls", namespace="lux_clothing")),
-    path("api/payments/", include("payment.urls", namespace="payment")),
-    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/doc/swagger/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path(
-        "api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
-    ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("api/user/", include("user.urls", namespace="user")),
+        path(
+            "api/lux_clothing/", include("lux_clothing.urls", namespace="lux_clothing")
+        ),
+        path("api/payments/", include("payment.urls", namespace="payment")),
+        path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "api/doc/swagger/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/doc/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + debug_toolbar_urls()
+)
